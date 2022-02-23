f262d1de73d0b69ef30df65f2cfce930150bd859
Edmar Zatarin <edzatarin@gmail.com>
2022-02-23 11:51:02 -0300

    Update BookEnricher.cs
    Alterado nome da classe RensponseTypeFormat para o nome correto ResponseTypeFormat.

--- SampleProject/RESTFulSampleServer/HyperMedia/BookEnricher.cs
+++ SampleProject/RESTFulSampleServer/HyperMedia/BookEnricher.cs
@@ -17,21 +17,21 @@ namespace RESTFulSampleServer.HyperMedia
                 Action = HttpActionVerb.GET,
                 Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
-                Type = RensponseTypeFormat.DefaultGet
+                Type = ReponseTypeFormat.DefaultGet
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.POST,
                 Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
-                Type = RensponseTypeFormat.DefaultPost
+                Type = ResponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.PUT,
                 Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
-                Type = RensponseTypeFormat.DefaultPost
+                Type = ReponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
